dba006f2d3ef5284507d13b371ead0373bb7e83c
Barrett Ford <barrett.ford@familysearch.org>
2020-10-07 11:05:08 -0600

    Added psucontrol command
    Also figured out some validation stuff, will need to do more
    validation.

--- octoprint_usb_keyboard/static/js/usb_keyboard.js
+++ octoprint_usb_keyboard/static/js/usb_keyboard.js
@@ -336,6 +336,54 @@ $(function() {
     });
     
     
+    ko.extenders.numeric = function(target, precision) {
+        //create a writable computed observable to intercept writes to our observable
+        var result = ko.pureComputed({
+            read: target,  //always return the original observables value
+            write: function(newValue) {
+                var current = target(),
+                    roundingMultiplier = Math.pow(10, precision),
+                    newValueAsNum = isNaN(newValue) ? 0 : +newValue,
+                    valueToWrite = Math.round(newValueAsNum * roundingMultiplier) / roundingMultiplier;
+ 
+                //only write if it changed
+                if (valueToWrite !== current) {
+                    target(valueToWrite);
+                } else {
+                    //if the rounded value is the same, but a different value was written, force a notification for the current field
+                    if (newValue !== current) {
+                        target.notifySubscribers(valueToWrite);
+                    }
+                }
+            }
+        }).extend({ notify: 'always' });
+ 
+        //initialize with current value to make sure it is rounded appropriately
+        result(target());
+ 
+        //return the new computed observable
+        return result;
+    };
+    
+    function CommandsCommandPluginPsucontrolViewModel(params) {
+      var self = this
+      Lockable.call(self, "action", params.locked)
+
+      // console.log("CommandsCommandPluginPsucontrolViewModel raw", params)
+      // console.log("CommandsCommandPluginPsucontrolViewModel self", self)
+      
+      self.profile = params.profile;
+      self.type = params.commandActionObject.type;
+      self.command = params.commandActionObject.command;
+      self.hotendMax = params.commandActionObject.hotend_max.extend({ numeric: 0 });
+      self.supportedCommands = ["on", "off", "toggle"]
+    }
+    ko.components.register('sfr-commands-command-plugin-psucontrol', {
+      viewModel: CommandsCommandPluginPsucontrolViewModel,
+      template: { element: 'template-sfr-commands-command-plugin-psucontrol' }
+    });
+    
+    
     function ProfileViewModel(params) {
       var self = this
       Lockable.call(self, "profile")
